03233a5c23fb8d32654353f9398cbcd341343894
Mantasha Q <69576802+MantashaQamar@users.noreply.github.com>
2022-10-30 00:13:49 +0530

    Add files via upload
    Lecture 2(If_else, While_loop)
    Scope of Variables

---
+++ src/Scope/Scope1.java
@@ -0,0 +1,33 @@
+package Scope;
+
+public class Scope1 {
+
+	public static void main(String[] args) {
+		int i = 10;
+		System.out.println(i);
+		if(i == 10) {
+			int n = 23;
+			System.out.println(i);
+			System.out.println(n);
+		}else {
+			int n = 232;
+		}
+		int n = 4;
+		System.out.println(n);
+		
+		i = 1;
+		while(i <= n) {
+			int j = 1;
+			while(j <= i) {
+			System.out.print(j);
+			j = j + 1;
+		}
+			System.out.println();
+			i = i + 1;
+	}
+
+	}
+}
+//Use only after declaration
+//No two variables with same name can coexist
+//Scope of variable is linited to surrounding block
