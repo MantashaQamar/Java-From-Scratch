167545a3bd12ddcad7d08b7f97c7fec00d16bd3a
Mantasha Q <69576802+MantashaQamar@users.noreply.github.com>
2022-11-01 23:45:52 +0530

    Lecture 5
    Patterns

---
+++ src/Lecture5/Pattern1usingForLoop.java
@@ -0,0 +1,19 @@
+package Lecture5;
+import java.util.Scanner;
+public class Pattern1usingForLoop {
+
+	public static void main(String[] args) {
+		 Scanner s = new Scanner(System.in);
+		  int n = s.nextInt();
+		  int i,j;
+		  for(i=1;i<=n;i++)
+          {
+          for(j=1;j<=i;j++)
+           System.out.print(i);
+         System.out.println("");
+  }
+	}
+
+}
+
+
---
+++ src/Lecture5/SquarePattern.java
@@ -0,0 +1,24 @@
+package Lecture5;
+import java.util.Scanner;
+
+public class SquarePattern {
+
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+	        int N = sc.nextInt();
+	        
+			
+		int i=1;
+		while (i<=N) {
+			int j =1;
+			while(j<=N){
+				System.out.print( N );
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+
+	}
+}
+
